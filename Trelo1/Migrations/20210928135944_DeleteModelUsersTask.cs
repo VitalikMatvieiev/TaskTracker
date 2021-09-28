0793bfaf0b8a82cfb2d51abe41bff785cd64368d
Vitalii Matvieiev <vitalik.matveev133@gmail.com>
2021-09-28 17:02:04 +0300

    Delete UserTasks model

---
+++ Trelo1/Migrations/20210928135944_DeleteModelUsersTask.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Trelo1.Migrations
+{
+    public partial class DeleteModelUsersTask : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UsersTasks");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UsersTasks",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    UserTaskId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.ForeignKey(
+                        name: "FK_UsersTasks_Tasks_UserTaskId",
+                        column: x => x.UserTaskId,
+                        principalTable: "Tasks",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UsersTasks_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsersTasks_UserId",
+                table: "UsersTasks",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsersTasks_UserTaskId",
+                table: "UsersTasks",
+                column: "UserTaskId");
+        }
+    }
+}
