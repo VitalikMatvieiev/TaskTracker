b661a219e63b74ba07167a7330021135df3620b4
Vitalii Matvieiev <vitalik.matveev133@gmail.com>
2021-09-29 14:41:40 +0300

    Add foring key

---
+++ Trelo1/Migrations/20210929112547_AddForingKeysToTable.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Trelo1.Migrations
+{
+    public partial class AddForingKeysToTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Boards_Organizations_OrganizationId",
+                table: "Boards");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Boards_BoardId",
+                table: "Tasks");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BoardId",
+                table: "Tasks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OrganizationId",
+                table: "Boards",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Boards",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Boards_Organizations_OrganizationId",
+                table: "Boards",
+                column: "OrganizationId",
+                principalTable: "Organizations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Boards_BoardId",
+                table: "Tasks",
+                column: "BoardId",
+                principalTable: "Boards",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Boards_Organizations_OrganizationId",
+                table: "Boards");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Boards_BoardId",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Boards");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BoardId",
+                table: "Tasks",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "OrganizationId",
+                table: "Boards",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Boards_Organizations_OrganizationId",
+                table: "Boards",
+                column: "OrganizationId",
+                principalTable: "Organizations",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Boards_BoardId",
+                table: "Tasks",
+                column: "BoardId",
+                principalTable: "Boards",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
--- Trelo1/Models/Board.cs
+++ Trelo1/Models/Board.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,13 @@ namespace Trelo1.Models
     {
         [Key]
         public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [ForeignKey("OrganizationId")]
+        [Required]
+        public int OrganizationId { get; set; }
+        public Organization Organization { get; set; }
         
         public List<UserTask> UserTasks { get; set; }
         
