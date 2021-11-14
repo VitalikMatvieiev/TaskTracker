faa064019f23356c1922b015a42767184084d957
Vitalii Matvieiev <vitalik.matveev133@gmail.com>
2021-11-14 10:43:01 +0200

    Add method for hashing password

--- TreloBLL/Services/UserService.cs
+++ TreloBLL/Services/UserService.cs
@@ -11,6 +11,7 @@ using System.Threading.Tasks;
 using System.Text;
 using Microsoft.AspNetCore.Http;
 using TreloBLL.Interfaces;
+using System.Security.Cryptography;
 
 namespace Trelo1.Services
 {
@@ -133,6 +134,46 @@ namespace Trelo1.Services
             {
                 return null;
             }
+        }
+
+        public string HashUserPassword(string password)
+        {
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 100000);
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            byte[] hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+
+            string savedPasswordHash = Convert.ToBase64String(hashBytes);
+
+            return savedPasswordHash;
+        }
+        public async Task<bool> CheckUserHashPassword(string Email, string password)
+        {
+            var user = await _dbContext.Users.FirstOrDefaultAsync(u => u.Email == Email);
+            string userPass = user.Password;
+            /* Extract the bytes */
+            byte[] hashBytes = Convert.FromBase64String(userPass);
+            /* Get the salt */
+            byte[] salt = new byte[16];
+            Array.Copy(hashBytes, 0, salt, 0, 16);
+            /* Compute the hash on the password the user entered */
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 100000);
+            byte[] hash = pbkdf2.GetBytes(20);
+            /* Compare the results */
+            for (int i = 0; i < 20; i++)
+            {
+                if (hashBytes[i + 16] != hash[i])
+                {
+                    throw new UnauthorizedAccessException();
+                }
+            }
+            return true;
+                
         }
 
     }
